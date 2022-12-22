71d2abd9c8a330206d97fa87aeb66ed4792f2862
christian-rummey <christian.rummey@gmail.com>
2022-12-22 14:31:38 +0100

    Updated: Visit Barcharts & Timeline Graphs

--- FACHILD.graph.Visits.Timeline.R
+++ FACHILD.graph.Visits.Timeline.R
@@ -4,7 +4,7 @@ dt.   <- readRDS('DATA derived/dt.long.rds')
 dt. %<>% 
   group_by(sjid) %>% 
   mutate(startdt = min(adt)) %>% 
-  left_join(.dd.FA('demo') %>% select(sjid, site))
+  left_join(.dd.atx('demo') %>% select(sjid, site))
 
 dt.$sjid <- fct_reorder(dt.$sjid, dt.$startdt, min)
 
@@ -15,11 +15,11 @@ virt.vis <- dt. %>%
   # mutate(hpf = ifelse( is.na(hpf), 'filler', as.character(hpf))) %>% 
   select( -fpf ) %>% 
   filter  ( !grepl('.iu', paramcd) ) %>% 
-  select  ( study, sjid, adt, param, hpf) %>% 
-  group_by( study, sjid, adt, param) %>%
+  select  ( study, sjid, adt, paramcd, hpf) %>% 
+  group_by( study, sjid, adt, paramcd) %>%
   filter  ( hpf %in% c('Audio Only','Video')) %>% 
   ungroup %>% 
-  select  ( -param, -hpf, -param) %>% 
+  select  ( -paramcd, -hpf) %>% 
   unique %>% 
   arrange( adt ) %>% 
   # filter (!(sjid == 5035 & adt == as.Date('2019-02-25'))) %>% 
@@ -29,28 +29,25 @@ virt.vis <- dt. %>%
 p1 <- dt. %>%
   filter(study == 'FACHILD') %>% 
   ggplot()+
+  lemon::geom_pointline( shorten = .1 ) +
   aes(x = adt, y = sjid)+
   aes(group = sjid, color = site)+
-  # aes(shape = virtual)+scale_shape_manual(values=c(21,19))+
-  # scale_y_discrete()+
-  geom_point()+geom_line()+
+  facet_wrap(~study)+
   theme_minimal(base_size = 16)+
   theme(panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank())+
   theme(axis.text.y = element_blank())+.leg_tl+
   xlab('Date')+ylab('Subjects')+
-  facet_wrap(~study)+
   ggtitle('Follow-up Timeline')+theme(plot.title = element_text(hjust = 0.5))
-
 p1
 
 p2 <- dt. %>%
   filter(study == 'FACHILD') %>% 
   left_join(virt.vis) %>% mutate(virtual = ifelse(is.na(virtual), 'In-Person', virtual)) %>% 
   ggplot()+
-  geom_point( size = 2 ) + geom_line( ) +
+  lemon::geom_pointline( shorten = .1 ) +
   aes(x = adt, y = sjid)+
   aes(group = sjid, color = site)+
-  aes(shape = virtual)+scale_shape_manual(values=c(21,19))+
+  aes(shape = virtual)+scale_shape_manual(values=c(3,19))+
   # scale_y_discrete()+
   theme_minimal(base_size = 16)+
   theme(panel.grid.minor.y = element_blank(), panel.grid.major.y = element_blank())+
